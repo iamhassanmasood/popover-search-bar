f1c8477de606e24818c81f9d0483b71e83508998
Hassan Masood <iamhassanmasood@gmail.com>
2022-11-08 17:04:44 +0500

    task:popover search bar created with material ui

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Input,
+  Slide,
+  Button,
+  InputAdornment,
+  ClickAwayListener,
+} from "@mui/material";
+import cssStyles from "../../utils/cssStyles";
+import Iconify from "../Iconify";
+import IconButtonAnimate from "../IconButtonAnimate";
+
+const SearchbarStyle = styled("div")(({ theme }) => ({
+  ...cssStyles(theme).bgBlur(),
+  top: 0,
+  left: 0,
+  zIndex: 99,
+  width: "100%",
+  display: "flex",
+  position: "absolute",
+  alignItems: "center",
+  height: 64,
+  boxShadow: "0 8px 16px 0 rgba(145, 158, 171, 0.16)",
+}));
+
+export default function Searchbar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <ClickAwayListener onClickAway={handleClose}>
+      <div>
+        {!isOpen && (
+          <IconButtonAnimate onClick={handleOpen}>
+            <Iconify icon={"eva:search-fill"} width={20} height={20} />
+          </IconButtonAnimate>
+        )}
+
+        <Slide direction="down" in={isOpen} mountOnEnter unmountOnExit>
+          <SearchbarStyle>
+            <Input
+              autoFocus
+              fullWidth
+              disableUnderline
+              placeholder="Search…"
+              startAdornment={
+                <InputAdornment position="start">
+                  <Iconify
+                    icon={"eva:search-fill"}
+                    sx={{ color: "text.disabled", width: 20, height: 20 }}
+                  />
+                </InputAdornment>
+              }
+              sx={{ mr: 1, fontWeight: "fontWeightBold" }}
+            />
+            <Button variant="contained" onClick={handleClose}>
+              Search
+            </Button>
+          </SearchbarStyle>
+        </Slide>
+      </div>
+    </ClickAwayListener>
+  );
+}
